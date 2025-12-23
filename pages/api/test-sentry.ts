baf980bf72a47e3d1a5179ac3b2b8afead4844ea
Berry <jobspeakpro@gmail.com>
2025-12-23 12:11:19 -0500

    Add Sentry test endpoint (App Router)

--- pages/api/test-sentry.ts
+++
@@ -1,9 +0,0 @@
-﻿import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.query.test === 'true') {
-    throw new Error('Sentry test error – server side');
-  }
-
-  res.status(200).json({ ok: true });
-}
