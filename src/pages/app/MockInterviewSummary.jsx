aee41bdaba9a3a3aa7ac9360670227caaf924bd7
Berry <jobspeakpro@gmail.com>
2026-01-03 07:57:12 -0500

    fix: replace AppHeader with UniversalHeader in MockInterviewSummary and PracticeSummary to fix ReferenceError

--- src/pages/app/MockInterviewSummary.jsx
+++ src/pages/app/MockInterviewSummary.jsx
@@ -689,7 +689,7 @@ export default function MockInterviewSummary() {
 
     return (
         <div className="bg-background-light dark:bg-background-dark min-h-screen flex flex-col">
-            <AppHeader />
+            <UniversalHeader />
 
             <main className="flex-1 w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col gap-6">
                 {/* Header */}
